c74c6ac58400560e0ed10924c4b2f10211ba5715
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-24 15:17:34 +0530

    [pranav] Add. Created binary search tree.

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,11 +8,6 @@ namespace BinarySearchTreeProgram
         {
             Console.WriteLine("Welcome to Binary Search Tree Program");
             BinaryTree<int> binaryTree = new BinaryTree<int>();
-            binaryTree.addToData(56);
-            binaryTree.addToData(30);
-            binaryTree.addToData(70);
-
-            binaryTree.display(binaryTree.getRoot());
         }
     }
 }
