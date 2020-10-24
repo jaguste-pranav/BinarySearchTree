dde0f24db05749ce3f1615be451bfeb05c0f0e31
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-24 15:15:41 +0530

    [pranav] Add. Created binary search tree.

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTreeProgram
+{
+    class BinaryTree<T> where T : IComparable
+    {
+        Node<T> root;
+        Node<T> current;
+        int count = 0;
+        bool result = false;
+        public void addToData(T data)
+        {
+            if (root == null)
+            {
+                this.root = new Node<T>(data);
+                this.current = root;
+                count++;
+                return;
+            }
+            if (this.current.NodeValue.CompareTo(data) > 0)
+            {
+                if (this.current.LeftNode == null)
+                {
+                    this.current.LeftNode = new Node<T>(data);
+                    this.current = root;
+                    count++;
+
+                }
+                else
+                {
+                    this.current = this.current.LeftNode;
+                    addToData(data);
+                }
+            }
+            else
+            {
+                if (this.current.RightNode == null)
+                {
+                    this.current.RightNode = new Node<T>(data);
+                    this.current = root;
+                    count++;
+                }
+                else
+                {
+                    this.current = this.current.RightNode;
+                    addToData(data);
+                }
+            }
+
+        }
+
+        
+        public Node<T> getRoot()
+        {
+            return this.root;
+        }
+        public void display(Node<T> node)
+        {
+            if (node != null)
+            {
+                display(node.LeftNode);
+                Console.WriteLine(" " + node.NodeValue);
+                display(node.RightNode);
+            }
+
+        }
+        public int getSize()
+        {
+            return count;
+        }
+
+        public bool search(int data, Node<T> node)
+        {
+
+            if (node == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (this.current.NodeValue.Equals(data))
+                {
+                    result = true;
+                }
+                else if (this.current.NodeValue.CompareTo(data) > 0)
+                {
+                    this.current = this.current.LeftNode;
+                    search(data, current);
+                }
+                else
+                {
+                    this.current = this.current.RightNode;
+                    search(data, current);
+                }
+
+                return result;
+            }
+        }
+
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinarySearchTreeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+            BinaryTree<int> binaryTree = new BinaryTree<int>();
+            binaryTree.addToData(56);
+            binaryTree.addToData(30);
+            binaryTree.addToData(70);
+
+            binaryTree.display(binaryTree.getRoot());
+        }
+    }
+}
