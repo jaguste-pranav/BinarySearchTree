623284c1e3b2236fa7e940cd46604363789bbdb1
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-24 15:20:09 +0530

    [pranav] Add. Add data to binary search tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,6 +8,16 @@ namespace BinarySearchTreeProgram
         {
             Console.WriteLine("Welcome to Binary Search Tree Program");
             BinaryTree<int> binaryTree = new BinaryTree<int>();
+
+            binaryTree.addToData(56);
+            binaryTree.addToData(30);
+            binaryTree.addToData(70);
+
+            binaryTree.addToData(22);
+            binaryTree.addToData(40);
+            binaryTree.addToData(95);
+
+            binaryTree.display(binaryTree.getRoot());
         }
     }
 }
